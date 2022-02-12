4080f5b4ef3fd93f845431d52d2f9a6c38c461c2
Jarxay <99241070+Jarxay@users.noreply.github.com>
2022-02-12 18:46:27 +0100

    MGU 2.1

--- NinjaLegends.lua
+++ NinjaLegends.lua
@@ -1,4 +1,9 @@
+--MGU Made by jarxay
+
+local DiscordInvite = "https://discord.gg/7XFnezqGc3"
+local Version = 2.1
 Library = loadstring(game:HttpGet('https://lindseyhost.com/UI/LinoriaLib.lua'))();
+Library:Notify('e')
 local plr = game:GetService("Players").LocalPlayer
 local mouse = plr:GetMouse()
 local Fonts = {};
@@ -13,7 +18,12 @@ local JumpStuffArgs = {
 }
 
 
-
+local function PrintIslands()
+    for i,v in pairs(game:GetService("Workspace").islandUnlockParts:GetChildren()) do
+        print(v.Name)
+        wait()
+    end
+end
 
 local GroundSlamargs = {
     [1] = "groundSlam"
@@ -42,12 +52,12 @@ local function BlazingVortexFunc()
 end;
 
 local function ManualSell()
+    local ManCurrentPos = plr.Character.HumanoidRootPart.CFrame
     plr.Character.HumanoidRootPart.CFrame = CFrame.new(85,91245,116)
+    wait(0.2)
+    plr.Character.HumanoidRootPart.CFrame = ManCurrentPos
 end;
 
-local function ManualSwing()
-    game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(args))
-end;
 
 local function DebugFunc()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Jarxay/SimpleSpy/main/main.lua"))()
@@ -60,28 +70,26 @@ end;
 local function KickPLR()
     plr:Kick("Why'd you even click this lol")
 end;
-local TestWindow = Library:CreateWindow("MGU : gg/SyRVxBdNYa");
-Library:SetWatermark('MGU : '.. plr.Name.. ' : Release');
+local TestWindow = Library:CreateWindow("MGU  "..DiscordInvite);
+Library:SetWatermark('MGU | '..Version ' | '.. plr.Name.. ' | Release');
 Library:Notify('Loading UI...');
 
 local MainTab = TestWindow:AddTab('Main');
 local PlayerTab = TestWindow:AddTab('Player');
 local TeleportTab = TestWindow:AddTab('Teleports');
-local MainTabBox1 = MainTab:AddLeftTabbox('Swing');
-local MainTabBox2 = MainTab:AddRightTabbox('Sell');
-local MainTabBox3 = MainTab:AddRightTabbox('Unlock all islands');
-local Swing = MainTabBox1:AddTab('Swing');
-local SellTabber = MainTabBox2:AddTab('Sell');
-local UnlockAll = MainTabBox3:AddTab('Unlock all islands');
+local MainTabBox1 = MainTab:AddLeftTabbox('Auto');
+local MainTabBox2 = MainTab:AddRightTabbox('Unlock all islands');
+local SellTabber = MainTabBox1:AddTab('Auto');
+local UnlockAll = MainTabBox2:AddTab('Unlock all islands');
 local MiscTab = TestWindow:AddTab('Misc');
 local SettingsTab = TestWindow:AddTab('Settings');
-Swing:AddButton('Manual Swing', ManualSwing)
-Swing:AddToggle('Autoswing', { Text = 'Autoswing' });
-SellTabber:AddButton('Manual Sell',ManualSell)
+
+SellTabber:AddToggle('Autoswing', { Text = 'Autoswing' });
 SellTabber:AddToggle('Autosell', { Text = 'Autosell' });
-UnlockAll:AddButton('Unlock all islands', TeleportAll);
-
-
+SellTabber:AddToggle('AutoBuySword', {Text = 'Autobuy sword'});
+SellTabber:AddToggle('AutoBuyBelt', {Text = 'Autobuy belts'})
+SellTabber:AddToggle('AutoRebirth', {Text = 'Auto Rebirth'})
+UnlockAll:AddButton('Unlock all islands', TeleportAll)
 local TeleportBox1 = TeleportTab:AddLeftTabbox('Spawn');
 local TeleportText2 = TeleportBox1:AddTab('Spawn');
 TeleportText2:AddButton('Spawn', SpawnTPFunc);
@@ -96,6 +104,7 @@ FunText1:AddToggle('ParticleSpamToggle', { Text = 'Particles spam loop'})
 local FunBox2 = MiscTab:AddRightTabbox('Debug');
 local FunText2 = FunBox2:AddTab('Debug')
 
+UnlockAll:AddButton('Manual sell', ManualSell)
 FunText2:AddButton('RemoteEvents',DebugFunc);
 FunText2:AddButton('PrintPos', PrintPosFunc);
 FunText2:AddButton('KickPLR', KickPLR)
@@ -279,5 +288,438 @@ Toggles.DuelTimer:OnChanged(function()
         end
     end
 end)
+
+
+
+Toggles.AutoBuySword:OnChanged(function()
+    while Toggles.AutoBuySword.Value == true do
+        wait()
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons.Ground:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs1 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs1))
+            wait(0.01)
+        end
+        wait(0.01)
+
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Astral Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs2 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs2))
+            wait(0.01)
+        end
+        wait(0.01)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Space Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs3 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs3))
+            wait(0.01)
+        end
+        wait(0.01)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Tundra Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs4 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs4))
+            wait(0.01)
+        end
+
+        wait(0.01)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Eternal Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs5 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs5))
+            wait(0.01)
+        end
+        wait()
+
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Sandstorm"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs6 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs6))
+            wait(0.01)
+        end
+        wait()
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Thunderstorm"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs7 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs7))
+            wait(0.01)
+        end
+        wait()
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Ancient Inferno Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs8 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs8))
+            wait(0.01)
+        end
+
+        wait(0.01)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Midnight Shadow Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs9 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs9))
+            wait(0.0001)
+        end
+        wait(0.01)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Mythical Souls Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs10 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs10))
+            wait(0.0001)
+        end
+        wait(0.01)
+
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Winter Wonder Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs11 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs11))
+            wait(0.0001)
+        end
+
+        wait()
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Golden Master Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs12 = {
+                [1] = "buySword",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs12))
+            wait(0)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Dragon Legend Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs13 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs13))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Cybernetic Legends Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs14 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs14))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Skystorm Ultraus Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs15 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs15))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Chaos Legends Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs16 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs16))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Soul Fusion Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs17 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs17))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Dark Elements Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs18 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs18))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Inner Peace Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs19 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs19))
+            wait(0.0001)
+        end
+        wait(0.15)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Weapons["Blazing Vortex Island"]:GetChildren()) do
+            print(v.Name)
+            local BuySwordArgs20 = {
+                [1] = "buySword",
+                [2] = v.Name,
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuySwordArgs20))
+            wait(0)
+        end
+
+        local ihateblacks = game:GetService("ReplicatedStorage").Weapons["Ancient Inferno Island"]["DW Ancient Assassin Blade"]
+        local ArgsForBlackPeople = {
+            [1] = "buySword",
+            [2] = ihateblacks
+        }
+        
+        game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(ArgsForBlackPeople))
+    end
+end)
+
+Toggles.AutoBuyBelt:OnChanged(function()
+    while Toggles.AutoBuyBelt.Value == true do
+        wait()
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts.Ground:GetChildren()) do
+            local BuyBeltArgs1 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs1))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Astral Island"]:GetChildren()) do
+            local BuyBeltArgs2 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs2))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Space Island"]:GetChildren()) do
+            local BuyBeltArgs3 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs3))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Tundra Island"]:GetChildren()) do
+            local BuyBeltArgs4 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs4))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Eternal Island"]:GetChildren()) do
+            local BuyBeltArgs5 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs5))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Sandstorm"]:GetChildren()) do
+            local BuyBeltArgs6 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs6))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Thunderstorm"]:GetChildren()) do
+            local BuyBeltArgs7 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs7))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Ancient Inferno Island"]:GetChildren()) do
+            local BuyBeltArgs8 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs8))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Midnight Shadow Island"]:GetChildren()) do
+            local BuyBeltArgs9 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs9))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Mythical Souls Island"]:GetChildren()) do
+            local BuyBeltArgs10 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs10))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Winter Wonder Island"]:GetChildren()) do
+            local BuyBeltArgs11 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs11))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Golden Master Island"]:GetChildren()) do
+            local BuyBeltArgs12 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs12))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Dragon Legend Island"]:GetChildren()) do
+            local BuyBeltArgs13 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs13))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Cybernetic Legends Island"]:GetChildren()) do
+            local BuyBeltArgs14 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs14))
+        end
+
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Skystorm Ultraus Island"]:GetChildren()) do
+            local BuyBeltArgs15 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs15))
+        end
+
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Soul Fusion Island"]:GetChildren()) do
+            local BuyBeltArgs16 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs16))
+        end
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Chaos Legends Island"]:GetChildren()) do
+            local BuyBeltArgs15 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs15))
+        end
+        wait(0.1)
+
+        wait(0.1)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Belts["Dark Elements Island"]:GetChildren()) do
+            local BuyBeltArgs17 = {
+            [1] = "buyBelt",
+            [2] = v.Name
+            }
+        
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(BuyBeltArgs17))
+        end
+
+
+    end
+end)
+
+Toggles.AutoRebirth:OnChanged(function()
+    while Toggles.AutoRebirth.Value == true do
+        wait(0.01)
+        for i,v in pairs(game:GetService("ReplicatedStorage").Ranks.Ground:GetChildren()) do
+            local args = {
+                [1] = "buyRank",
+                [2] = v.Name
+            }
+            
+            game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(unpack(args))
+            wait(0.001)
+        end
+    end
+end)
 UpdateTheme()
-Library:Notify('Jarxay#0001 : youtube.com/c/jarxay');
+Library:Notify('Made by Jarxay#0001 | '.. DiscordInvite);
+
+
+
+
